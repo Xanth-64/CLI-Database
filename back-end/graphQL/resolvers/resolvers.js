8d08e436ac277a86f2d78e17c8968214550abea5
Andres Eloy <30258079@correo.unimet.edu.ve>
2021-02-08 01:17:12 -0400

    Extra BackEnd Queries Implemented

--- back-end/graphQL/resolvers/resolvers.js
+++ back-end/graphQL/resolvers/resolvers.js
@@ -1,4 +1,7 @@
+const { Sequelize } = require('sequelize')
+
 // Resolvers
+const Op = Sequelize.Op
 const resolvers = {
     Query: {
         async getPersonas(root, args, { models }) {
@@ -11,6 +14,88 @@ const resolvers = {
 
         async getEdificios(root, args, { models }) {
             return await models.edificio.findAll()
+        },
+
+        async getPersonaByCedula(root, { extranjeria, numero_ci }, { models }) {
+            return await models.persona.findOne({
+                where: {
+                    extranjeria: extranjeria,
+                    numero_ci: numero_ci,
+                },
+            })
+        },
+
+        async getAreaComun(root, { numero, tipo }, { models }) {
+            return await models.areacomun.findOne({
+                where: {
+                    numero: numero,
+                    tipo: tipo,
+                },
+            })
+        },
+
+        async getEdificio(root, { numero, nombre_conjunto }, { models }) {
+            return await models.edificio.findOne({
+                where: {
+                    numero: numero,
+                    nombre_conjunto: nombre_conjunto,
+                },
+            })
+        },
+    },
+    Mutation: {
+        async createEdificio(root, { nombre_conjunto, numero }, { models }) {
+            return await models.edificio.create({ nombre_conjunto, numero })
+        },
+
+        async createPersona(
+            root,
+            { nombre, apellido, extranjeria, numero_ci, edad },
+            { models }
+        ) {
+            return await models.persona.create({
+                nombre,
+                apellido,
+                extranjeria,
+                numero_ci,
+                edad,
+            })
+        },
+
+        async createAreaComun(root, { tipo, numero }, { models }) {
+            return models.areacomun.create({
+                estado: 'Disponible',
+                tipo: tipo,
+                numero: numero,
+            })
+        },
+
+        async updatePersonaName(
+            root,
+            { extranjeria, numero_ci, newNombre },
+            { models }
+        ) {
+            return await models.persona.update(
+                { nombre: newNombre },
+                {
+                    where: {
+                        extranjeria: extranjeria,
+                        numero_ci: numero_ci,
+                    },
+                }
+            )[1]
+        },
+
+        async occupyAreaComun(root, { personaID, momento, tipo, numero }) {
+            return await models.areacomun.update(
+                { personaID, momento },
+                {
+                    where: {
+                        tipo: tipo,
+                        numero: numero,
+                    },
+                }
+            )[1]
         },
     },
 }
--- back-end/graphQL/typeDefs/typeDefs.js
+++ back-end/graphQL/typeDefs/typeDefs.js
@@ -15,8 +15,8 @@ const typeDefs = gql`
         estado: String!
         tipo: String!
         numero: Int!
-        personaID: Int!
-        momento: Int!
+        personaID: Int
+        momento: Int
     }
 
     type edificio {
@@ -29,15 +29,37 @@ const typeDefs = gql`
         getPersonas: [persona]
         getAreas: [areacomun]
         getEdificios: [edificio]
-        getPersonaByCedula(extranjeria String!,numero_ci Int!): persona
-        getAreaComun(numero Int!, tipo String!): areacomun
-        
+        getPersonaByCedula(extranjeria: String!, numero_ci: Int!): persona!
+        getAreaComun(numero: Int!, tipo: String!): areacomun!
+        getEdificio(numero: Int!, nombre_conjunto: String!): edificio!
     }
 
-    type Mutations {
+    type Mutation {
+        createEdificio(nombre_conjunto: String!, numero: Int!): edificio!
+        createPersona(
+            nombre: String!
+            apellido: String!
+            extranjeria: String!
+            numero_ci: Int!
+            edad: Int!
+        ): persona!
+        createAreaComun(tipo: String!, numero: Int!): areacomun!
 
+        updatePersonaName(
+            extranjeria: String!
+            numero_ci: Int!
+            newNombre: String!
+        ): [persona!]
+
+        occupyAreaComun(
+            personaID: Int!
+            momento: Int!
+            tipo: String!
+            numero: Int!
+        ): [areacomun!]
+
+        deOccupyAreaComun(tipo: String!, numero: Int!): [areacomun!]
     }
-
 `
 
 module.exports = typeDefs

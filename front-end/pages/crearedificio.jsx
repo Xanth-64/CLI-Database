c1094bbbed40b138b018679a6e56e1aeae296a3d
Andres Eloy <30258079@correo.unimet.edu.ve>
2021-03-09 02:32:13 -0400

    More Stufffff

--- front-end/pages/crearedificio.jsx
+++ front-end/pages/crearedificio.jsx
@@ -1,12 +1,24 @@
 import { useForm } from 'react-hook-form'
+import { useMutation, gql } from '@apollo/client'
 const crearedificio = () => {
+    const createEDF = gql`
+        mutation createEdificio($nombre_conjunto: String!, $numero: Int!) {
+            createEdificio(nombre_conjunto: $nombre_conjunto, numero: $numero) {
+                numero
+            }
+        }
+    `
+
+    const [createEdificio, { loading, error, data }] = useMutation(createEDF)
+
     const { register, handleSubmit } = useForm()
     const onSubmit = (data) => {
-        if (data.numero.isInteger()) {
-            data.numero = parseInt(data.numero)
-            console.log(data)
-            //PONER EL QUERY AQUI
-        }
+        createEdificio({
+            variables: {
+                nombre_conjunto: data.nombre_conjunto,
+                numero: parseInt(data.numero),
+            },
+        })
     }
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -24,6 +36,11 @@ const crearedificio = () => {
                 <input ref={register} name="numero" type="number" id="numero" />
             </div>
             <button type="submit"> CREAR EDIFICIO</button>
+            {data == undefined ? (
+                <div>Intente crear un Edificio</div>
+            ) : (
+                <div>Edificio Creado Exitosamente</div>
+            )}
         </form>
     )
 }
